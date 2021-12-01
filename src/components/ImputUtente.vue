bd0907ada1462291d9ce024732046f6195f9b91f
ValerioMik <valerio.avallone@hotmail.com>
2021-12-01 16:16:13 +0100

    step 2 quasi terminato

--- src/components/ImputUtente.vue
+++ src/components/ImputUtente.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="imput">
-    <input type="text" placeholder="Inserisci un film">
-    <button>CERCA</button>
-    <CardFilm v-for="films,i in listadeiFilm"
+    <input type="text" placeholder="Inserisci un film" v-model.trim="imputText">
+    <button @click.prevent="testotrovato">CERCA</button>
+    <CardFilm v-for="films,i in filtralalistapersonaggi"
     :key="i"
     :details="films"/>
   </div>
@@ -19,12 +19,24 @@ export default {
   },
   data(){
     return{
-      apiUrl:"https://api.themoviedb.org/3/search/movie?api_key=c766fe019c512e0736766f4ff20690b2&query=Spider+man",
+      apiUrl:"https://api.themoviedb.org/3/search/movie?api_key=c766fe019c512e0736766f4ff20690b2&query="+this.ricercatext,
        listadeiFilm:[],
+       imputText:"",
+       ricercatext:""
     }
   },
   created(){
    this.informazioniFilm();
+  },
+  computed:{
+    filtralalistapersonaggi(){
+      if(this.ricercatext === ""){
+        return this.listadeiFilm
+      }
+      return this.listadeiFilm.filter((item)=>{
+        return item.title.toLowerCase().icludes(this.ricercatext.toLowerCase())
+      })
+    }
   },
   methods:{
     informazioniFilm(){
@@ -34,6 +46,10 @@ export default {
         this.listadeiFilm = result.data.results
        console.log(result.data.results);
       })
+    },
+    testotrovato(){
+      this.ricercatext = this.imputText;
+      console.log(this.ricercatext);
     }
   }
 }
