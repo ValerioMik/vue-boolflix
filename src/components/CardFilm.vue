781e250caedef06c83fe2ace320d67fa3ef13f4b
ValerioMik <valerio.avallone@hotmail.com>
2021-12-02 17:25:17 +0100

    Fix bug immagini mileston 3

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -3,7 +3,7 @@
     <h2>Film</h2>
     <h2>{{ details.title }}</h2>
     <h3>{{ details.original_title }}</h3>
-    <span><img :src="bandieraImmagini()" /></span>
+    <span><img :src="bandieraImmagini()"/></span>
     <br />
     <span>{{ details.vote_average }}</span>
     <img :src="importaImmagini()" :alt="details.original_title">
@@ -20,23 +20,26 @@ export default {
       details:Object,
   },
   methods:{
-  bandieraImmagini(){
-      if(this.details.original_language === 'it'){
-        return "https://www.buy-spares.ie/sites/responsive/img/universal/flags/it-flag.png"
-      }else if(this.details.original_language === 'en'){
-        return "https://th.bing.com/th/id/R.8a8a985a6266b52f32e326ef17603955?rik=uoYiPLaWj9Yt0g&riu=http%3a%2f%2fwww.rgledhill.co.uk%2fimages%2fgb_flag.gif&ehk=IYDSLs30Co0xTYL1Zh9%2bhzwBPjI3S2IfGi5SQdjVkro%3d&risl=&pid=ImgRaw&r=0"
-      }else if(this.details.original_language === 'es'){
-        return "https://th.bing.com/th/id/R.c5d09f69c2b8c99438e176963abbdecb?rik=VMtAUlZ9%2bXaBvg&riu=http%3a%2f%2fwww.dsalenia.it%2fimages%2fflag_spanish.gif&ehk=5Ezc4PsSThB1njE2MDMbZIwCKiIDKn4YwL0Xo7EJNZc%3d&risl=&pid=ImgRaw&r=0"
-      }else "not-found"
+    bandieraImmagini(){
+        if(this.details.original_language === 'it'){
+          return "https://www.buy-spares.ie/sites/responsive/img/universal/flags/it-flag.png"
+        }else if(this.details.original_language === 'en'){
+          return "https://th.bing.com/th/id/R.8a8a985a6266b52f32e326ef17603955?rik=uoYiPLaWj9Yt0g&riu=http%3a%2f%2fwww.rgledhill.co.uk%2fimages%2fgb_flag.gif&ehk=IYDSLs30Co0xTYL1Zh9%2bhzwBPjI3S2IfGi5SQdjVkro%3d&risl=&pid=ImgRaw&r=0"
+        }else if(this.details.original_language === 'es'){
+          return "https://th.bing.com/th/id/R.c5d09f69c2b8c99438e176963abbdecb?rik=VMtAUlZ9%2bXaBvg&riu=http%3a%2f%2fwww.dsalenia.it%2fimages%2fflag_spanish.gif&ehk=5Ezc4PsSThB1njE2MDMbZIwCKiIDKn4YwL0Xo7EJNZc%3d&risl=&pid=ImgRaw&r=0"
+        }else {
+          return "not-found"
+        }
+    },
+    importaImmagini(){
+      if(this.details.detposter_path !== null){
+        return `https://image.tmdb.org/t/p/w342${this.details.poster_path}`
+      }else if (this.details.poster_path === null){
+        return `https://th.bing.com/th/id/R.eab98c7b1c8fe293ab4e1ecfe30cba4b?rik=Zr9x5Ys05B1Skw&pid=ImgRaw&r=0&sres=1&sresct=1`
+      }
     }
   },
-  importaImmagini(){
-    if(this.details.poster_path !== null){
-      return `https://image.tmdb.org/t/p/w342${this.details.poster_path}`
-    }else if (this.details.poster_path === null){
-      return `https://th.bing.com/th/id/OIP.AC9frN1qFnn-I2JCycN8fwHaEK?pid=ImgDet&rs=1`
-    }
-  }
+ 
 }
 </script>
 
--- src/components/CardSerie.vue
+++ src/components/CardSerie.vue
@@ -1,39 +1,45 @@
 <template>
   <div id="dettagli">
-      <h2>Serie</h2>
+    <h2>Serie</h2>
     <h2>{{ details.name }}</h2>
     <h3>{{ details.original_name }}</h3>
     <span><img :src="bandieraImmagini()" /></span>
     <br />
+    <img :src="importaImmagini()" :alt="details.original_title">
     <span>{{ details.vote_average }}</span>
   </div>
 </template>
 
 
 <script>
-
-
 export default {
-  name: 'CardSerie',
+  name: "CardSerie",
   props: {
-      details:Object,
+    details: Object,
   },
-  methods:{
-  bandieraImmagini(){
-      if(this.details.original_language === 'it'){
-        return "https://www.buy-spares.ie/sites/responsive/img/universal/flags/it-flag.png"
-      }else if(this.details.original_language === 'en'){
-        return "https://th.bing.com/th/id/R.8a8a985a6266b52f32e326ef17603955?rik=uoYiPLaWj9Yt0g&riu=http%3a%2f%2fwww.rgledhill.co.uk%2fimages%2fgb_flag.gif&ehk=IYDSLs30Co0xTYL1Zh9%2bhzwBPjI3S2IfGi5SQdjVkro%3d&risl=&pid=ImgRaw&r=0"
-      }else if(this.details.original_language === 'es'){
-        return "https://th.bing.com/th/id/R.c5d09f69c2b8c99438e176963abbdecb?rik=VMtAUlZ9%2bXaBvg&riu=http%3a%2f%2fwww.dsalenia.it%2fimages%2fflag_spanish.gif&ehk=5Ezc4PsSThB1njE2MDMbZIwCKiIDKn4YwL0Xo7EJNZc%3d&risl=&pid=ImgRaw&r=0"
-      }else "not-found"
+  methods: {
+    bandieraImmagini() {
+      if (this.details.original_language === "it") {
+        return "https://www.buy-spares.ie/sites/responsive/img/universal/flags/it-flag.png";
+      } else if (this.details.original_language === "en") {
+        return "https://th.bing.com/th/id/R.8a8a985a6266b52f32e326ef17603955?rik=uoYiPLaWj9Yt0g&riu=http%3a%2f%2fwww.rgledhill.co.uk%2fimages%2fgb_flag.gif&ehk=IYDSLs30Co0xTYL1Zh9%2bhzwBPjI3S2IfGi5SQdjVkro%3d&risl=&pid=ImgRaw&r=0";
+      } else if (this.details.original_language === "es") {
+        return "https://th.bing.com/th/id/R.c5d09f69c2b8c99438e176963abbdecb?rik=VMtAUlZ9%2bXaBvg&riu=http%3a%2f%2fwww.dsalenia.it%2fimages%2fflag_spanish.gif&ehk=5Ezc4PsSThB1njE2MDMbZIwCKiIDKn4YwL0Xo7EJNZc%3d&risl=&pid=ImgRaw&r=0";
+      } else "not-found";
+    },
+    importaImmagini(){
+      if(this.details.detposter_path !== null){
+        return `https://image.tmdb.org/t/p/w342${this.details.poster_path}`
+      }else if (this.details.poster_path === null){
+        return `https://th.bing.com/th/id/R.eab98c7b1c8fe293ab4e1ecfe30cba4b?rik=Zr9x5Ys05B1Skw&pid=ImgRaw&r=0&sres=1&sresct=1`
+      }
     }
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss">
-#dettagli{
-    border: 2px solid black;
+#dettagli {
+  border: 2px solid black;
 }
 </style>
